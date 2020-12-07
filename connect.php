12aecdc845b1b908ec63f55fc1aeb2433551d98d
laxmipriya0496 <75523014+laxmipriya0496@users.noreply.github.com>
2020-12-07 17:57:09 +0530

    07-12

--- connect.php
+++ connect.php
@@ -1,10 +1,13 @@
 <?php
 define("Title", 'Recruitment');
 try {
-	$con = new pdo ('mysql:host=localhost;dbname=hr','root','Best2020Know');
+	$con = new pdo ('mysql:host=localhost;dbname=hr','root','');
 } 
 catch (Exception $e) 
 {
 	echo $e->getMessage();
 }
 ?>
+<!--?php
+echo "test";
+?-->
